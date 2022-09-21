e51152bf323bc5480fdb758cf4aab5b944df5931
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-09-21 13:35:55 -0500

    timer html

--- quiz.js
+++ quiz.js
@@ -1,3 +1,18 @@
-//Create reference for start button
-var generateBtn = document.querySelector("#start");
+//Required elements
+var startquiz_btn= document.QuerySelector(".start_quiz");
 
+
+
+var startTimer()
+}
+
+
+
+
+
+-
+//When start quiz button is clicked
+
+startquiz_btn.addEventListener("click,") {
+ setInterval
+}
