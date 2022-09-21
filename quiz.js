802351bf5f57ae67f2ffab65fae4f2adc2f326a7
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-09-21 14:54:56 -0500

    countdown timer

--- quiz.js
+++ quiz.js
@@ -1,18 +1,27 @@
-//Required elements
-var startquiz_btn= document.QuerySelector(".start_quiz");
+//Selects element by class
+var timerEl = (docuement.querySelector = "timer");
 
+//Putting the timer into a variable
+function countdown() {
+  var secondsRemain = 60;
 
+  var gameInterval = setInterval(function () {
+    
+    if (secondsRemain > 1) {
+      timerEl.textContent = secondsRemain + "seconds remaining";
+      secondsRemain--;
+    } else if (secondsRemain === 1) {
+      timerEl.textContent + "second remaining";
 
-var startTimer()
+      secondsRemain--;
+    } else {
+      timerEl.textContent = "";
+
+      clearInterval(gameInterval);
+      displayCountdown()
+    }
+  }, 6000);
 }
 
 
-
-
-
--
-//When start quiz button is clicked
-
-startquiz_btn.addEventListener("click,") {
- setInterval
-}
+countdown();
